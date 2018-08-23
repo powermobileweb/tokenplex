6bc34569e65433fcb8721d69f842f4753518eabc
PowerMobile Team <info@powermobile.org>
2018-08-23 22:10:48 +0200

    add ccxt integration

---
+++ app.js
@@ -0,0 +1,62 @@
+// PowerMobile Team tokenplex
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var create_table = require('./routes/schema');
+
+const cron_route_api = require('./routes/cron');
+const coin_detail_api = require('./routes/coins');
+const exchange_api = require('./routes/exchange');
+
+var cors = require('cors')
+
+require('dotenv').config()
+
+var app = express();
+app.use(cors())
+app.use(express.static('public'))
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/coin', coin_detail_api);
+app.use('/create', create_table);
+app.use('/exchange', exchange_api);
+app.use('/cron', cron_route_api);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
