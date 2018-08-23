16e43fbc24e3041a816b1b391463baef4dd95778
PowerMobile Team <info@powermobile.org>
2018-08-23 22:12:23 +0200

    add integration tests

---
+++ config/development.js
@@ -0,0 +1,10 @@
+// PowerMobile Team TokenPlex
+
+'use strict';
+
+module.exports = {
+  env: 'development',
+  db: 'mongodb://localhost/Tododb',
+  port: process.env.PORT || 4000,
+};
+
---
+++ config/production.js
@@ -0,0 +1,1 @@
+// PowerMobile Team TokenPlex
